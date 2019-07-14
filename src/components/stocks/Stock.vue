1175daef585bf2e7b554fbd0f0028788259b79be
Mariia <1907mss@gmail.com>
2019-07-14 17:35:27 +0300

    cteating stocks components

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,39 @@
 <template>
-  <h1>The Stock</h1>
+  <div class="card">
+    <div class="card-header text-primary">
+      <h3 class="card-title">
+        Name
+        <small>(Price: Price)</small>
+      </h3>
+    </div>
+    <div class="card-body text-primary">
+      <div class="pull-left">
+        <input
+          type="number"
+          class="form-control"
+          placeholder="Quantity"
+        >
+      </div>
+      <div class="pull-right">
+        <button
+          class="btn btn-primary"
+          >Buy</button>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style>
+  .card {
+    width: 48%;
+    padding: 15px;
+    margin-bottom: 40px;
+    margin-right: 30px;
+    border: 1px solid #337ab7;
+    border-radius: 5px;
+  }
+
+  .card-header {
+    margin-bottom: 15px;
+  }
+</style>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,32 @@
 <template>
-  <h1>The Stocks</h1>
+  <div class="container-stocks">
+    <app-stock v-for="stock in stocks" :key="stock"></app-stock>
+  </div>
 </template>
+
+<script>
+  import Stock from "./Stock.vue";
+  export default {
+    data() {
+      return {
+        stocks: [
+          { id: 1, name: "BMW", price: 100 },
+          { id: 2, name: "Google", price: 200 },
+          { id: 3, name: "Apple", price: 300 },
+          { id: 4, name: "Twitter", price: 400 }
+        ]
+      }
+    },
+    components: {
+      appStock: Stock
+    }
+  }
+</script>
+
+<style scope>
+  .container-stocks {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+</style>
