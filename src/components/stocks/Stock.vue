da3f721672b58c2bb9f7d66e972b7c40550fa70e
Mariia <1907mss@gmail.com>
2019-07-14 18:32:25 +0300

    buy button ia added

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -2,8 +2,8 @@
   <div class="card">
     <div class="card-header text-primary">
       <h3 class="card-title">
-        Name
-        <small>(Price: Price)</small>
+        {{ stock.name }}
+        <small>(Price: {{ stock.price }})</small>
       </h3>
     </div>
     <div class="card-body text-primary">
@@ -12,16 +12,41 @@
           type="number"
           class="form-control"
           placeholder="Quantity"
+          v-model.number="quantity"
         >
       </div>
       <div class="pull-right">
         <button
           class="btn btn-primary"
+          @click="buyStock"
+          :disabled="quantity<=0 || !Number.isInteger(quantity)"
           >Buy</button>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        quantity: 0
+      }
+    },
+    props: ['stock'],
+    methods: {
+      buyStock() {
+        const order = {
+          stockId: this.stock.id,
+          stockPrice: this.stock.price,
+          quantity: this.quantity
+        };
+        console.log(order);
+        this.quantity = 0;
+      }
+    }
+  }
+</script>
 
 <style>
   .card {
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container-stocks">
-    <app-stock v-for="stock in stocks" :key="stock"></app-stock>
+    <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
   </div>
 </template>
 
