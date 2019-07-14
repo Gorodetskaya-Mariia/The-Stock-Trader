f0443d85db1dad46a408b0bd6c638eda32d3969e
Mariia <1907mss@gmail.com>
2019-07-14 19:10:42 +0300

    setting up the Vuex State Management

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,24 +1,19 @@
 <template>
   <div class="container-stocks">
-    <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
+    <app-stock v-for="stock in stocks" :key="stock" :stock="stock"></app-stock>
   </div>
 </template>
 
 <script>
   import Stock from "./Stock.vue";
   export default {
-    data() {
-      return {
-        stocks: [
-          { id: 1, name: "BMW", price: 100 },
-          { id: 2, name: "Google", price: 200 },
-          { id: 3, name: "Apple", price: 300 },
-          { id: 4, name: "Twitter", price: 400 }
-        ]
-      }
-    },
     components: {
       appStock: Stock
+    },
+    computed: {
+      stocks() {
+        return this.$store.getters.stocks;
+      }
     }
   }
 </script>
